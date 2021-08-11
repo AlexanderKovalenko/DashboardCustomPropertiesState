8e065ad32d4f9252f7730c737e3cb6af34bdb757
AlexanderKovalenko <alexander.kovalenko@devexpress.com>
2021-08-11 17:18:24 +0300

    Initial commit

---
+++ Default.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="Default" %>
+
+<!DOCTYPE html>
+
+<html>
+<head runat="server">
+    <title>Test</title>
+
+    <style type="text/css">
+        html, body, form {  
+            height: 100%;  
+            margin: 0;  
+            padding: 0;  
+            overflow: hidden;  
+        }
+    </style>
+
+    <script type="text/javascript">
+        function onBeforeRender(sender) {
+            var control = sender.GetDashboardControl();
+            control.registerExtension(new DashboardStateExtension(control))
+        }
+    </script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <dx:ASPxDashboard ID="ASPxDashboard1" runat="server" DashboardStorageFolder="~/App_Data/Dashboards" Height="100%"
+            OnDashboardLoading="ASPxDashboard1_DashboardLoading">
+            <ClientSideEvents BeforeRender="onBeforeRender" />
+        </dx:ASPxDashboard>
+    </form>
+    <script src="DashboardStateExtension.js"></script>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,32 @@
+﻿using DevExpress.DashboardCommon;
+using DevExpress.DashboardWeb;
+using System;
+
+public partial class Default : System.Web.UI.Page {
+    protected void Page_Load(object sender, EventArgs e) {
+        ASPxDashboard1.SetDataSourceStorage(CreateDataSourceStorage());
+    }
+
+    public DataSourceInMemoryStorage CreateDataSourceStorage() {
+        DataSourceInMemoryStorage dataSourceStorage = new DataSourceInMemoryStorage();          
+        DashboardObjectDataSource objDataSource = new DashboardObjectDataSource("ObjectDataSource", typeof(ProductSales));
+
+        objDataSource.DataMember = "GetProductSales";
+        dataSourceStorage.RegisterDataSource("objectDataSource", objDataSource.SaveToXml());
+
+        return dataSourceStorage;
+    }
+
+    protected void ASPxDashboard1_DashboardLoading(object sender, DashboardLoadingWebEventArgs e) {
+        if (e.DashboardId == "dashboard1") {
+            Dashboard dashboard = new Dashboard();
+            dashboard.LoadFromXDocument(e.DashboardXml);
+
+            var stateFromXml = dashboard.CustomProperties.GetValue("DashboardState");
+
+            dashboard.CustomProperties.SetValue("DashboardState", "{\"Items\":{\"gridDashboardItem1\":{\"MasterFilterValues\":[[2,\"Chai\"]]}}}");
+
+            e.DashboardXml = dashboard.SaveToXDocument();
+        }
+    }
+}
